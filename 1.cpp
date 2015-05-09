9eba5d5a39da6303387fc52f92796fd5f81e1423
Benjamin BONTEMPS <zigaroula@hotmail.fr>
2015-05-09 14:54:13 +0200

    Légères modif 1.cpp, pas encore bon

--- 1.cpp
+++ 1.cpp
@@ -4,56 +4,64 @@ using namespace std;
 
 int main()
 {
-    int i,j;
-    int nx, ny;
-    float w;
-    float x[nx];
-    float y[ny];
+    while (1) {
+        int i,j;
+        int nx, ny;
+        float w;
 
-    float cutX[nx][2];
-    float cutY[ny][2];
+        cin >> nx >> ny >> w;
 
-    cin >> nx >> ny >> w;
-    for (i = 0 ; i < nx ; i++) {
-        cin >> x[i];
-        cutX[i][0] = x[i] - w;
-        cutX[i][1] = x[i] + w;
-    }
-    for (j = 0 ; j < ny ; j++) {
-        cin >> y[j];
-        cutY[j][0] = y[j] - w;
-        cutY[j][1] = y[j] + w;
-    }
-
-    for (i = 0 ; i < nx ; i++) {
-        for (j = 0 ; j < nx ; j++) {
-            if (cutX[i][1] >= cutX[j][0] && cutX[i][1] <= cutX[j][1] && cutX[i][0] <= cutX[j][0]) {
-                cutX[i][1] = cutX[j][1];
-                cutX[j][0] = cutX[i][0];
-            } else if (cutX[j][1] >= cutX[i][0] && cutX[j][1] <= cutX[i][1] && cutX[j][0] <= cutX[i][0]) {
-                cutX[i][0] = cutX[j][0];
-                cutX[j][1] = cutX[i][1];
-            }
+        if (nx == 0 && ny == 0 && w == 0.0) {
+            break;
         }
-        //cout << "Nouveau cutX : (" << cutX[i][0] << "," << cutX[i][1] << ")" << endl;
-    }
 
-    for (i = 0 ; i < ny ; i++) {
+        float x[nx];
+        float y[ny];
+
+        float cutX[nx][2];
+        float cutY[ny][2];
+
+        for (i = 0 ; i < nx ; i++) {
+            cin >> x[i];
+            cutX[i][0] = x[i] - w/2;
+            cutX[i][1] = x[i] + w/2;
+        }
         for (j = 0 ; j < ny ; j++) {
-            if (cutY[i][1] >= cutY[j][0] && cutY[i][1] <= cutY[j][1] && cutY[i][0] <= cutY[j][0]) {
-                cutY[i][1] = cutY[j][1];
-                cutY[j][0] = cutY[i][0];
-            } else if (cutY[j][1] >= cutY[i][0] && cutY[j][1] <= cutY[i][1] && cutY[j][0] <= cutY[i][0]) {
-                cutY[i][0] = cutY[j][0];
-                cutY[j][1] = cutY[i][1];
+            cin >> y[j];
+            cutY[j][0] = y[j] - w/2;
+            cutY[j][1] = y[j] + w/2;
+        }
+
+        for (i = 0 ; i < nx ; i++) {
+            for (j = 0 ; j < nx ; j++) {
+                if (cutX[i][1] >= cutX[j][0] && cutX[i][1] <= cutX[j][1] && cutX[i][0] <= cutX[j][0]) {
+                    cutX[i][1] = cutX[j][1];
+                    cutX[j][0] = cutX[i][0];
+                } else if (cutX[j][1] >= cutX[i][0] && cutX[j][1] <= cutX[i][1] && cutX[j][0] <= cutX[i][0]) {
+                    cutX[i][0] = cutX[j][0];
+                    cutX[j][1] = cutX[i][1];
+                }
+            }
+            //cout << "Nouveau cutX : (" << cutX[i][0] << "," << cutX[i][1] << ")" << endl;
+        }
+
+        for (i = 0 ; i < ny ; i++) {
+            for (j = 0 ; j < ny ; j++) {
+                if (cutY[i][1] >= cutY[j][0] && cutY[i][1] <= cutY[j][1] && cutY[i][0] <= cutY[j][0]) {
+                    cutY[i][1] = cutY[j][1];
+                    cutY[j][0] = cutY[i][0];
+                } else if (cutY[j][1] >= cutY[i][0] && cutY[j][1] <= cutY[i][1] && cutY[j][0] <= cutY[i][0]) {
+                    cutY[i][0] = cutY[j][0];
+                    cutY[j][1] = cutY[i][1];
+                }
             }
         }
-    }
 
-    if (cutX[0][0] <= 0.0 && cutX[0][1] >= 100.0 && cutY[0][0] <= 0.0 && cutY[0][1] >= 75.0) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
+        if (cutX[0][0] <= 0.0 && cutX[0][1] >= 100.0 && cutY[0][0] <= 0.0 && cutY[0][1] >= 75.0) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
