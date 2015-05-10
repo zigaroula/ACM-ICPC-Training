b8c13b17fdad1d95fb905d85cb5b729177b4362b
Benjamin BONTEMPS <zigaroula@hotmail.fr>
2015-05-10 15:33:28 +0200

    Resolution morse

--- 1.cpp
+++ 1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -23,41 +24,52 @@ int main()
 
         for (i = 0 ; i < nx ; i++) {
             cin >> x[i];
+        }
+        for (j = 0 ; j < ny ; j++) {
+            cin >> y[j];
+        }
+
+        sort(x, x + sizeof x / sizeof x[0]);
+        sort(y, y + sizeof y / sizeof y[0]);
+
+        for (i = 0 ; i < nx ; i++) {
             cutX[i][0] = x[i] - w/2;
             cutX[i][1] = x[i] + w/2;
         }
         for (j = 0 ; j < ny ; j++) {
-            cin >> y[j];
             cutY[j][0] = y[j] - w/2;
             cutY[j][1] = y[j] + w/2;
         }
 
-        for (i = 0 ; i < nx ; i++) {
-            for (j = 0 ; j < nx ; j++) {
-                if (cutX[i][1] >= cutX[j][0] && cutX[i][1] <= cutX[j][1] && cutX[i][0] <= cutX[j][0]) {
-                    cutX[i][1] = cutX[j][1];
-                    cutX[j][0] = cutX[i][0];
-                } else if (cutX[j][1] >= cutX[i][0] && cutX[j][1] <= cutX[i][1] && cutX[j][0] <= cutX[i][0]) {
-                    cutX[i][0] = cutX[j][0];
-                    cutX[j][1] = cutX[i][1];
-                }
-            }
-            //cout << "Nouveau cutX : (" << cutX[i][0] << "," << cutX[i][1] << ")" << endl;
-        }
+        int cptX = 0;
 
-        for (i = 0 ; i < ny ; i++) {
-            for (j = 0 ; j < ny ; j++) {
-                if (cutY[i][1] >= cutY[j][0] && cutY[i][1] <= cutY[j][1] && cutY[i][0] <= cutY[j][0]) {
-                    cutY[i][1] = cutY[j][1];
-                    cutY[j][0] = cutY[i][0];
-                } else if (cutY[j][1] >= cutY[i][0] && cutY[j][1] <= cutY[i][1] && cutY[j][0] <= cutY[i][0]) {
-                    cutY[i][0] = cutY[j][0];
-                    cutY[j][1] = cutY[i][1];
-                }
+        for (int i = 0 ; i < nx-1 ; i++) {
+            if (cutX[i][1]>=cutX[i+1][0]) {
+                cptX++;
+            } else {
+                break;
             }
         }
 
-        if (cutX[0][0] <= 0.0 && cutX[0][1] >= 100.0 && cutY[0][0] <= 0.0 && cutY[0][1] >= 75.0) {
+        if (cutX[0][0] <= 0.0 && cutX[nx-1][1] >= 75.0){
+            cptX++;
+        }
+
+        int cptY = 0;
+
+        for (int i = 0 ; i < ny-1 ; i++) {
+            if (cutY[i][1]>=cutY[i+1][0]) {
+                cptY++;
+            } else {
+                break;
+            }
+        }
+
+        if (cutY[0][0] <= 0.0 && cutY[ny-1][1] >= 100.0){
+            cptY++;
+        }
+
+        if (cptX==nx && cptY==ny) {
             cout << "YES" << endl;
         } else {
             cout << "NO" << endl;
--- 4.cpp
+++ 4.cpp
@@ -6,48 +6,85 @@ using namespace std;
 int cases, N;
 string code;
 vector<string> words;
-map<string,string> mapw;
+map<char,string> morse;
+vector<int> memo;
 
 void initMap() {
-    mapw["A"] = ".-";
-    mapw["B"] = "-...";
-    mapw["C"] = "-.-.";
-    mapw["D"] = "-..";
-    mapw["E"] = ".";
-    mapw["F"] = "..-.";
-    mapw["G"] = "--.";
-    mapw["H"] = "....";
-    mapw["I"] = "..";
-    mapw["J"] = ".---";
-    mapw["K"] = "-.-";
-    mapw["L"] = ".-..";
-    mapw["M"] = "--";
-    mapw["N"] = "-.";
-    mapw["O"] = "---";
-    mapw["P"] = ".--.";
-    mapw["Q"] = "--.-";
-    mapw["R"] = ".-.";
-    mapw["S"] = "...";
-    mapw["T"] = "-";
-    mapw["U"] = "..-";
-    mapw["V"] = "...-";
-    mapw["W"] = ".--";
-    mapw["X"] = "-..-";
-    mapw["Y"] = "-.--";
-    mapw["Z"] = "--..";
+    morse['A'] = ".-";
+    morse['B'] = "-...";
+    morse['C'] = "-.-.";
+    morse['D'] = "-..";
+    morse['E'] = ".";
+    morse['F'] = "..-.";
+    morse['G'] = "--.";
+    morse['H'] = "....";
+    morse['I'] = "..";
+    morse['J'] = ".---";
+    morse['K'] = "-.-";
+    morse['L'] = ".-..";
+    morse['M'] = "--";
+    morse['N'] = "-.";
+    morse['O'] = "---";
+    morse['P'] = ".--.";
+    morse['Q'] = "--.-";
+    morse['R'] = ".-.";
+    morse['S'] = "...";
+    morse['T'] = "-";
+    morse['U'] = "..-";
+    morse['V'] = "...-";
+    morse['W'] = ".--";
+    morse['X'] = "-..-";
+    morse['Y'] = "-.--";
+    morse['Z'] = "--..";
+}
+
+string coding(string s) {
+    string code = "";
+    for (int i = 0 ; i < s.size() ; i++) {
+        code += morse[s[i]];
+    }
+    return code;
+}
+
+int count(string& code, int pos) {
+    if (memo[pos] != -1) {
+        //cout << "coucou" << endl;
+        return memo[pos];
+    }
+    int number = 0;
+    for (int i = 0 ; i < words.size() ; i++) {
+        if (code.compare(pos, words[i].size(), words[i]) == 0) {
+            if (pos+words[i].size() == code.size()) {
+                number++;
+            } else {
+                number += count(code, pos+words[i].size());
+                //cout << pos << " " << number << endl;
+            }
+        }
+    }
+    memo[pos] = number;
+    return number;
 }
 
 int main() {
+    initMap();
     cin >> cases;
-    words.resize(10000);
     for (int numbercases = 0 ; numbercases < cases ; numbercases++) {
+        words.clear();
+        memo.clear();
+
         cin >> code >> N;
         for (int i = 0 ; i < N ; i++) {
             string word;
             cin >> word;
-            words[i] = word;
+            words.push_back(coding(word));
         }
-        initMap();
+
+        memo = vector<int>(code.size(), -1);
+
+        //vector<int> memo(code.size(), -1);
+
+        cout << count(code, 0) << endl;
 
         if (numbercases<cases-1) {
             cout << endl;
