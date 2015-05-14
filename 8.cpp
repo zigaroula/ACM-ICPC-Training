f65ebc8b8ac3bda29339c8a66f0d1c1ee0b8363f
Benjamin BONTEMPS <zigaroula@hotmail.fr>
2015-05-14 14:04:37 +0200

    8 almost done, time limit

--- 8.cpp
+++ 8.cpp
@@ -7,10 +7,36 @@ using namespace std;
 int cases, N;
 float V;
 vector<float> b, h, s;
+vector<vector<float> > cil;
+vector<vector<float> > v;
+
+void sortcil() {
+    while (!b.empty()) {
+        float base = b[0];
+        int minindice = 0;
+        for (int i = 0 ; i < b.size() ; i++) {
+            if (b[i]<base) {
+                base = b[i];
+                minindice = i;
+            }
+        }
+        vector<float> temp;
+        temp.push_back(b[minindice]);
+        temp.push_back(h[minindice]+b[minindice]);
+        temp.push_back(s[minindice]);
+        cil.push_back(temp);
+        b.erase(b.begin()+minindice);
+        h.erase(h.begin()+minindice);
+        s.erase(s.begin()+minindice);
+    }
+}
 
 int main() {
     cin >> cases;
     for (int numbercases = 0 ; numbercases < cases ; numbercases++) {
+
+        cil.clear();
+        v.clear();
 
         cin >> N;
         for (int i = 0 ; i < N ; i++) {
@@ -22,8 +48,36 @@ int main() {
         }
         cin >> V;
 
-        sort(b.begin(), b.end());
+        sortcil();
 
+        float x = 0;
+        float hmax = 0;
+        float inf, sup;
+        inf = 0;
+        sup = 0;
+        for (int i = 0 ; i < cil.size() ; i++) {
+            if (cil[i][1]>=sup) {
+                sup = cil[i][1];
+            }
+        }
+
+
+        while (x!=V) {
+            x=0;
+            hmax = (sup+inf)/2;
+            for (int i = 0 ; i<cil.size() ; i++) {
+                if (cil[i][0]<=hmax) {
+                    x += cil[i][2] * (min(hmax, cil[i][1]) - cil[i][0]);
+                }
+            }
+            if (x>V) {
+                sup = (sup+inf)/2;
+            } else {
+                inf = (sup+inf)/2;
+            }
+        }
+
+        cout << hmax << endl;
 
 /*
         for (int i = 0 ; i < N ; i++) {
