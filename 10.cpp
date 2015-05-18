e632f95261058ded0b5786259b5c8f389d5a0c5a
Benjamin BONTEMPS <zigaroula@hotmail.fr>
2015-05-18 14:23:43 +0200

    10 OK !

--- 10.cpp
+++ 10.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int cases, N, start, target;
 vector<vector<int> > rel;
-
+vector<int> tar;
+/*
 vector<int> mark, label;
 
 void initDijkstra() {
@@ -13,7 +14,7 @@ void initDijkstra() {
     mark.resize(N);
     label.clear();
     label.resize(N);
-    fill(label.begin(), label.end(), 200);
+    fill(label.begin(), label.end(), 100000);
     for (int i = 0 ; i < mark.size() ; i++) {
         mark[i] = i;
     }
@@ -23,8 +24,8 @@ void dijkstra() {
     initDijkstra();
     label[start] = 0;
 
-    while(!mark.empty()) {
-        int mindist = 200;
+    while(label[target]==100000) {
+        int mindist = 100001;
         int minindice = 0;
         int rawindice = 0;
         for (int j = 0 ; j < mark.size() ; j++) {
@@ -34,24 +35,27 @@ void dijkstra() {
                 rawindice = j;
             }
         }
+
         int a = minindice;
         mark.erase(mark.begin()+rawindice);
+
         for (int b = 0 ; b < rel[a].size() ; b++) {
-            if (find(rel[a].begin(), rel[a].end(), b)!=rel[a].end() && label[b] > label[a] + 1) {
-                label[b] = label[a]+1;
+            if (label[rel[a][b]] > label[a] + 1) {
+                label[rel[a][b]] = label[a]+1;
             }
         }
     }
 
     cout << start << " " << target << " " << label[target]-1 << endl;
 }
-
+*/
 int main() {
     cin >> cases;
     for (int numbercases = 0 ; numbercases < cases ; numbercases++) {
         start = 0;
         target = 0;
         rel.clear();
+        tar.clear();
 
         cin >> N;
 
@@ -70,7 +74,42 @@ int main() {
 
         cin >> start >> target;
 
-        dijkstra();
+        int cpt = 0;
+        int last = 0;
+        tar.push_back(start);
+        vector<bool> par(N, false);
+        par[start] = true;
+        bool stop = false;
+
+        while(1) {
+/*
+            for (int i = 0 ; i < tar.size() ; i++) {
+                cout << tar[i] << " ";
+            }
+            cout << endl;
+*/
+            int taille = tar.size();
+            for (int h = last ; h < taille ; h++) {
+                //cout << "EXAMIN DE " << tar[h] << endl;
+                for (int i = 0 ; i < rel[tar[h]].size() ; i++) {
+                    if (!par[rel[tar[h]][i]]) {
+                        tar.push_back(rel[tar[h]][i]);
+                        par[rel[tar[h]][i]] = true;
+                    }
+                }
+                if (par[target]) {
+                    stop = true;
+                    break;
+                }
+            }
+            last = taille;
+            if (stop) {
+                break;
+            }
+            cpt++;
+        }
+
+        cout << start << " " << target << " " << cpt << endl;
 
         if (numbercases<cases-1) {
             cout << endl;
