97c55799a07c1921c0fbaa471a236dc0effdec60
Benjamin BONTEMPS <zigaroula@hotmail.fr>
2015-06-16 23:29:40 +0200

    ok

--- 1348.cpp
+++ 1348.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <algorithm>
 #include <stdio.h>
+#include <sstream>
 using namespace std;
 
 #define LIMIT 10000
@@ -10,23 +11,23 @@ vector<string> commands;
 vector<vector<int> > args;
 bool infinity;
 
-int countMax(int adr, int limit) {
+int countMax(int adr, int limit, map<int, int> loopCount) {
     string com = commands[adr];
     if (limit < LIMIT) {
         if (com == "ifgo") {
-            return max(countMax(adr+1, limit+1) + 1, countMax(args[adr][0], limit+1) + 1);
+            return max(countMax(adr+1, limit+1, loopCount) + 1, countMax(args[adr][0], limit+1, loopCount) + 1);
         } else if (com == "jump") {
-            return countMax(args[adr][0], limit+1) + 1;
+            return countMax(args[adr][0], limit+1, loopCount) + 1;
         } else if (com == "pass") {
-            return countMax(adr+1, limit+1) + 1;
+            return countMax(adr+1, limit+1, loopCount) + 1;
         } else if (com == "loop") {
             //cout << "loop à l'adresse " << adr << " = " << args[adr][1] << endl;
-            if (args[adr][1] > 1) {
-                args[adr][1] --;
-                return countMax(args[adr][0], limit+1) + 1;
+            if (loopCount[adr] > 1) {
+                loopCount[adr] --;
+                return countMax(args[adr][0], limit+1, loopCount) + 1;
             } else {
-                args[adr][1] = args[adr][2];
-                return countMax(adr+1, limit+1) + 1;
+                loopCount[adr] = args[adr][1];
+                return countMax(adr+1, limit+1, loopCount) + 1;
             }
         } else if (com == "die") {
             return 1;
@@ -43,18 +44,24 @@ int main() {
         args.clear();
         infinity = false;
         while(1) {
+            string line;
+            getline(cin, line);
+            istringstream iss(line);
+            if (line.empty()) {
+                break;
+            }
             string com;
-            cin >> com;
+            iss >> com;
             if (com == "ifgo") {
                 int a;
-                cin >> a;
+                iss >> a;
                 commands.push_back(com);
                 vector<int> vec;
                 vec.push_back(a-1);
                 args.push_back(vec);
             } else if (com == "jump") {
                 int a;
-                cin >> a;
+                iss >> a;
                 commands.push_back(com);
                 vector<int> vec;
                 vec.push_back(a-1);
@@ -66,11 +73,10 @@ int main() {
                 args.push_back(vec);
             } else if (com == "loop") {
                 int a, b;
-                cin >> a >> b;
+                iss >> a >> b;
                 commands.push_back(com);
                 vector<int> vec;
                 vec.push_back(a-1);
-                vec.push_back(b);
                 vec.push_back(b);
                 args.push_back(vec);
             } else if (com == "die") {
@@ -78,27 +84,28 @@ int main() {
                 vector<int> vec;
                 vec.push_back(-1);
                 args.push_back(vec);
-                break;
             }
         }
 
+        if (cin.eof()) {
+            break;
+        }
+
+        map<int, int> loopCount;
+
+        for (int i = 0 ; i < commands.size() ; i++) {
+            if (commands[i] == "loop") {
+                loopCount[i] = args[i][1];
+            }
+        }
+
+        int res = countMax(0, 0, loopCount);
+
         if (!infinity) {
-            cout << countMax(0, 0) << endl;
+            cout << res << endl;
         } else {
             cout << "infinity" << endl;
         }
-
-        /*
-        for (int i = 0 ; i < commands.size() ; i++) {
-            cout << commands[i] << endl;
-        }
-        for (int i = 0 ; i < args.size() ; i++) {
-            for (int j = 0 ; j < args[i].size() ; j++) {
-                cout << args[i][j] << " ";
-            }
-            cout << endl;
-        }
-*/
 
     }
     return 0;
