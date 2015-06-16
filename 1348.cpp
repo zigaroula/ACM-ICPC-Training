a39ad808e0c8ea2eb5c17fbfe90b20bd2b8c4f9c
Benjamin BONTEMPS <zigaroula@hotmail.fr>
2015-06-16 18:35:51 +0200

    coucou

--- 1348.cpp
+++ 1348.cpp
@@ -5,10 +5,43 @@
 #include <stdio.h>
 using namespace std;
 
+#define LIMIT 10000
+vector<string> commands;
+vector<vector<int> > args;
+bool infinity;
+
+int countMax(int adr, int limit) {
+    string com = commands[adr];
+    if (limit < LIMIT) {
+        if (com == "ifgo") {
+            return max(countMax(adr+1, limit+1) + 1, countMax(args[adr][0], limit+1) + 1);
+        } else if (com == "jump") {
+            return countMax(args[adr][0], limit+1) + 1;
+        } else if (com == "pass") {
+            return countMax(adr+1, limit+1) + 1;
+        } else if (com == "loop") {
+            //cout << "loop à l'adresse " << adr << " = " << args[adr][1] << endl;
+            if (args[adr][1] > 1) {
+                args[adr][1] --;
+                return countMax(args[adr][0], limit+1) + 1;
+            } else {
+                args[adr][1] = args[adr][2];
+                return countMax(adr+1, limit+1) + 1;
+            }
+        } else if (com == "die") {
+            return 1;
+        }
+    } else {
+        infinity = true;
+        return 0;
+    }
+}
+
 int main() {
     while (1) {
-        vector<string> commands;
-        vector<vector<int> > args;
+        commands.clear();
+        args.clear();
+        infinity = false;
         while(1) {
             string com;
             cin >> com;
@@ -37,7 +70,8 @@ int main() {
                 commands.push_back(com);
                 vector<int> vec;
                 vec.push_back(a-1);
-                vec.push_back(b-1);
+                vec.push_back(b);
+                vec.push_back(b);
                 args.push_back(vec);
             } else if (com == "die") {
                 commands.push_back(com);
@@ -48,8 +82,13 @@ int main() {
             }
         }
 
+        if (!infinity) {
+            cout << countMax(0, 0) << endl;
+        } else {
+            cout << "infinity" << endl;
+        }
 
-
+        /*
         for (int i = 0 ; i < commands.size() ; i++) {
             cout << commands[i] << endl;
         }
@@ -59,7 +98,7 @@ int main() {
             }
             cout << endl;
         }
-
+*/
 
     }
     return 0;
